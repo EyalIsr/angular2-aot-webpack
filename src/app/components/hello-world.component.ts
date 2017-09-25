5ca919bbe377fc6b526752926894314ae5d0cd46
Administrator <Administrator@ESMASTEER>
2017-09-25 13:26:36 +0300

    my first commit with some bindings

--- src/app/components/hello-world.component.ts
+++ src/app/components/hello-world.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, HttpModule } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { Config } from '../config';
+
 
 @Component({
   selector: 'hello-world-app',
   templateUrl: 'hello-world.template.html',
+//  template:`
+//  <h1>Hello World! {{url}}</h1>
+//  <h2>hello hello </h2>
+//`,
   styleUrls: ['hello-world.style.css']
 })
-export class HelloWorldComponent {}
+export class HelloWorldComponent implements OnInit{
+
+    config: Config;
+    url: string;
+
+    ngOnInit(): void {
+        this.getJSON()
+            .then(config => {
+                this.config = config;
+                console.log(config.url);
+                this.url = config.url;
+            });
+        //console.log(this.config.url);
+    }
+    constructor(private http: Http) {
+       //this.url = 'lalalala';
+    }
+
+    public getJSON(): Promise<Config> {
+         return this.http.get("./config.json").toPromise()
+             .then(res =>
+                 res.json() as Config)
+             .catch(this.handleError);                            
+    }
+
+    private handleError(error: any): Promise<any> {
+     console.error('An error occurred', error); // for demo purposes only
+     return Promise.reject(error.message || error);
+}
+}
--- src/app/modules/main.module.ts
+++ src/app/modules/main.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HelloWorldComponent } from '../components/hello-world.component';
+import { HttpModule } from '@angular/http';
 
 export { HelloWorldComponent };
 
 @NgModule({
   bootstrap: [HelloWorldComponent],
   declarations: [HelloWorldComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpModule],
   providers: []
 })
 export class MainModule {}
